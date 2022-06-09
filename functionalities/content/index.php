44f4e1748b3af031a03f6961dcc2f689598b7aa2
Nonagod <nonagod.public@gmail.com>
2022-06-09 11:39:03 +0300

    1

--- functionalities/content/index.php
+++ functionalities/content/index.php
@@ -2,10 +2,23 @@
 require_once 'WSTDGetData.php';
 require_once 'shortcodes.php';
 
-// actions
+
+// ACTIONS
+
 add_action( 'wp', function() {
     global $ContentClass;
     $ContentClass = new WSTDGetData( get_the_ID( ));
 
     define('PAGE_TARGET_OBJECT_DATA', $ContentClass->page_data);
 });
+
+
+// FILTERS
+
+// turn off Gutenberg
+add_filter( 'use_block_editor_for_post_type', function( $use_block_editor, $post_type ){
+    if( $post_type === 'post_type' ) return false;
+
+    return $use_block_editor;
+}, 10, 2 );
+// add_filter('use_block_editor_for_post', '__return_false'); // for all posts

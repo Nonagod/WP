a97e602d1d69b7a5bc11e9a74bfadf5563118b79
Nonagod <nonagod.public@gmail.com>
2022-08-17 15:50:57 +0300

    1

--- functionalities/content/pagination.php
+++ functionalities/content/pagination.php
@@ -11,3 +11,14 @@ function initPaginationForPage( $page_name ) {
 // === initialization ===
 
 // initPaginationForPage( 'restaurants' );
+
+// === functions ===
+function printPagination( $number_of_pages, $active_page_number, $link_additional_classes = '', $link_format = 'page/%s' ) {
+    if( $number_of_pages > 1 ) {
+        ?><div class="pagination">
+            <?for($i = 1; $i <= $number_of_pages; $i++):?>
+                <a href="<?printf( $link_format, $i );?>" class="<?= $i === $active_page_number ? 'active' : ''?><?=' '.$link_additional_classes?>"></a>
+            <?endfor;?>
+        </div><?
+    }
+}
